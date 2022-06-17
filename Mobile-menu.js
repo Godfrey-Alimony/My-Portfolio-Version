c9cdeedc4f9fd84e5a16168543df8e87e49a7208
Godfrey-Alimony <alimonyg@gmail.com>
2022-06-17 23:01:44 +0300

    Add index.htm,styling file,javascript file,.hintrc,package.json,eslintrc file

--- Mobile-menu.js
+++ Mobile-menu.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+const menuIconMobile = document.querySelector('.navbar__hamb');
+const menu = document.querySelector('.navbar__menu');
+const viewport = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.navbar__menu__link');
+const heroLanding = document.querySelector('.hero');
+
+function isActiveClassExisted(element) {
+  return element.classList.contains('active');
+=======
 const menuIconMobile = document.querySelector(".navbar__hamb");
 const menu = document.querySelector(".navbar__menu");
 const viewport = document.querySelector("body");
@@ -6,10 +16,27 @@ const heroLanding = document.querySelector(".hero");
 
 function isActiveClassExisted(element) {
   return element.classList.contains("active");
+>>>>>>> 866879244e47474416deea41cc492a3f87f9be3c
 }
 
 function refreshIcon() {
   if (isActiveClassExisted(menu)) {
+<<<<<<< HEAD
+    menuIconMobile.setAttribute('src', 'images/hamburguer-close.png');
+  } else {
+    menuIconMobile.setAttribute('src', 'images/hamburguer.png');
+  }
+}
+function openMenu() {
+  if (menu.classList.contains('active')) {
+    menu.classList.toggle('active');
+    viewport.classList.add('blockover');
+    heroLanding.classList.remove('blur');
+  } else {
+    menu.classList.toggle('active');
+    viewport.classList.add('blockover');
+    heroLanding.classList.add('blur');
+=======
     menuIconMobile.setAttribute("src", "images/hamburguer-close.png");
   } else {
     menuIconMobile.setAttribute("src", "images/hamburguer.png");
@@ -25,11 +52,23 @@ function openMenu() {
     menu.classList.toggle("active");
     viewport.classList.add("blockover");
     heroLanding.classList.add("blur");
+>>>>>>> 866879244e47474416deea41cc492a3f87f9be3c
   }
   refreshIcon();
 }
 
 function closeMenu() {
+<<<<<<< HEAD
+  menu.classList.remove('active');
+  viewport.classList.remove('blockover');
+  refreshIcon();
+}
+
+menuIconMobile.addEventListener('click', openMenu);
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', closeMenu);
+});
+=======
   menu.classList.remove("active");
   viewport.classList.remove("blockover");
   refreshIcon();
@@ -39,3 +78,4 @@ menuIconMobile.addEventListener("click", openMenu);
 menuLinks.forEach((menuLink) => {
   menuLink.addEventListener("click", closeMenu);
 });
+>>>>>>> 866879244e47474416deea41cc492a3f87f9be3c
